b5dfb1d70302bc9c70081fa206a3ea35ac2a9b50
Rushabh Gandhi <rushabh.dg@somaiya.edu>
2023-04-08 14:38:53 +0530

    ui fix

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
+    if request.method=="POST":
+        print(request.FILES)
+        return redirect('home')
     return render(request, 'main/home.html')
